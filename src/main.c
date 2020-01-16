320d09b6e27fbf90a3801a636cb269186c535d68
Aleksandr Volkov <ramwoolf@gmail.com>
2020-01-16 23:50:22 +0300

    LED Blink by std periph

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
 #include "stm32f4xx.h"
 
 int main(void) {
-
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-    GPIOD->MODER = 0x55000000;
-    GPIOD->OTYPER = 0;
-    GPIOD->OSPEEDR = 0;
     
     uint32_t i = 0u;
+    GPIO_InitTypeDef GPIO_Init_LED;
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+    GPIO_Init_LED.GPIO_Pin = GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+    GPIO_Init_LED.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_Init_LED.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init_LED.GPIO_OType = GPIO_OType_PP;
+    GPIO_Init_LED.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+    GPIO_Init(GPIOD, &GPIO_Init_LED);
 
     while (1) {
-        GPIOD->ODR = 0xF000;
+        GPIO_SetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
         for (i = 0u; i < 2000000; ++i){}
-        GPIOD->ODR = 0x0000;
+        GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
         for (i = 0u; i < 2000000; ++i){}
     }
 }
